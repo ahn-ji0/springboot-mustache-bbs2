db0edcbb46c140cb71805eb7048437f1164a8429
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-10 14:14:36 +0900

    Add: variable-Long id, method-toEntity(with id version)

--- src/main/java/com/mustache/bbspractice2/domain/ArticleDto.java
+++ src/main/java/com/mustache/bbspractice2/domain/ArticleDto.java
@@ -3,13 +3,21 @@ package com.mustache.bbspractice2.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class ArticleDto {
-    String title;
-    String content;
+    private Long id;
+    private String title;
+    private String content;
+
+    public ArticleDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
     public ArticleEntity toEntity(){
         return new ArticleEntity(title,content);
+    }
+    public ArticleEntity toEntity(Long id){
+        return new ArticleEntity(id,title,content);
     }
 }
