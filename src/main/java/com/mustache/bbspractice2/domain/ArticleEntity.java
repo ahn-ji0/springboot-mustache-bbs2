e121c5e26726082980ec844a1e6b276c95d607ac
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-10 12:00:43 +0900

    Add : NoArgsConstructor

--- src/main/java/com/mustache/bbspractice2/domain/ArticleEntity.java
+++ src/main/java/com/mustache/bbspractice2/domain/ArticleEntity.java
@@ -2,10 +2,12 @@ package com.mustache.bbspractice2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @AllArgsConstructor
+@NoArgsConstructor // 안하면 findAll 할때 no default constructor for entity 오류
 @Getter
 @Entity
 @Table(name = "article4")
