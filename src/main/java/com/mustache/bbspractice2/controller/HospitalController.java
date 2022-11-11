b7a30ac0b06984326a2eb87662f0125bd7d33ee2
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-11 14:21:37 +0900

    Add: model attribute to be passed

--- src/main/java/com/mustache/bbspractice2/controller/HospitalController.java
+++ src/main/java/com/mustache/bbspractice2/controller/HospitalController.java
@@ -2,6 +2,11 @@ package com.mustache.bbspractice2.controller;
 
 import com.mustache.bbspractice2.domain.entity.HospitalEntity;
 import com.mustache.bbspractice2.respository.HospitalRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +16,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/hospitals")
+@Slf4j
 public class HospitalController {
 
     private final HospitalRepository hospitalRepository;
@@ -19,10 +25,15 @@ public class HospitalController {
         this.hospitalRepository = hospitalRepository;
     }
 
-    @GetMapping("/list")
-    public String listAll(Model model){
-        List<HospitalEntity> hospitals = hospitalRepository.findAll();
-        model.addAttribute("hospitals",hospitals);
+    @GetMapping("/")
+    public String listAll(Model model, @PageableDefault(size = 20, sort="id",direction=Sort.Direction.ASC) Pageable pageable){
+        Page<HospitalEntity> hospitals = hospitalRepository.findAll(pageable);
+        //log.info(hospitals.getPageable().toString()); = Page request [number: 0, size 20, sort: id: ASC]
+        //log.info(String.valueOf(hospitals.getNumberOfElements())); = 20
+        //log.info(String.valueOf(hospitals.getTotalPages())); = 4951
+        model.addAttribute("hospitals",hospitals.getContent());
+        model.addAttribute("previousPage", hospitals.previousOrFirstPageable().getPageNumber());
+        model.addAttribute("nextPage",hospitals.nextOrLastPageable().getPageNumber());
         return "hospitals/list";
     }
 }
