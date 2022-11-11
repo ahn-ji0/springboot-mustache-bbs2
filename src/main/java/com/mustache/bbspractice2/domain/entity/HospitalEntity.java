40d7300cf0206d4cdfcb7c8f89624f8ea7c6c3a9
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-11 14:22:11 +0900

    Fix: 필요한 정보를 담은 column만 포함하도록 변경

--- src/main/java/com/mustache/bbspractice2/domain/entity/HospitalEntity.java
+++ src/main/java/com/mustache/bbspractice2/domain/entity/HospitalEntity.java
@@ -15,19 +15,10 @@ import java.time.LocalDateTime;
 public class HospitalEntity {
     @Id
     private Integer id;
-    private String openServiceName;
-    private int openLocalGovernmentCode;
-    private String managementNumber;
-    private LocalDateTime licenseDate;
-    private int businessStatus;
-    private int businessStatusCode;
-    private String phone;
-    private String fullAddress;
+
+    @Column(name = "road_name_address")
     private String roadNameAddress;
+
+    @Column(name = "hospital_name")
     private String hospitalName;
-    private String businessTypeName;
-    private int healthcareProviderCount;
-    private int patientRoomCount;
-    private int totalNumberOfBeds;
-    private float totalAreaSize;
 }
