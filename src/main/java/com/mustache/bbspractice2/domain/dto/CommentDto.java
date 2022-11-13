5c4a4a83fa9cb3fc58ea329b8e248c46670fa9ad
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-13 17:35:19 +0900

    Update: content를 commentContent로 변경(article과 이름 중복), toEntity 메소드 추가

--- src/main/java/com/mustache/bbspractice2/domain/dto/CommentDto.java
+++ src/main/java/com/mustache/bbspractice2/domain/dto/CommentDto.java
@@ -1,6 +1,7 @@
 package com.mustache.bbspractice2.domain.dto;
 
 import com.mustache.bbspractice2.domain.entity.ArticleEntity;
+import com.mustache.bbspractice2.domain.entity.CommentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,5 +9,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CommentDto {
     private String userName;
-    private String content;
+    private String commentContent;
+
+    public CommentEntity toEntity(ArticleEntity articleEntity){
+        return new CommentEntity(this.userName,this.commentContent,articleEntity);
+    }
 }
