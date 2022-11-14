c7620521244de4bd1cced699c687daca6aa5aab1
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-14 14:17:09 +0900

    Add: findByBuisnessTypeNameIn

--- src/main/java/com/mustache/bbspractice2/respository/HospitalRepository.java
+++ src/main/java/com/mustache/bbspractice2/respository/HospitalRepository.java
@@ -4,10 +4,11 @@ import com.mustache.bbspractice2.domain.entity.ArticleEntity;
 import com.mustache.bbspractice2.domain.entity.CommentEntity;
 import com.mustache.bbspractice2.domain.entity.HospitalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<HospitalEntity,Integer> {
-    List<HospitalEntity> findByHospitalName(String hospitalName);
-
+    List<HospitalEntity> findByBusinessTypeNameIn(List<String> businessTypes);
 }
