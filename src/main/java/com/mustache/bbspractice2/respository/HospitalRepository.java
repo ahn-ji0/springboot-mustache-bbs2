0d98d3497f3adf1fd4ce804626f30c0d070fc497
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-14 13:22:08 +0900

    Add: findByHospitalName

--- src/main/java/com/mustache/bbspractice2/respository/HospitalRepository.java
+++ src/main/java/com/mustache/bbspractice2/respository/HospitalRepository.java
@@ -1,8 +1,13 @@
 package com.mustache.bbspractice2.respository;
 
 import com.mustache.bbspractice2.domain.entity.ArticleEntity;
+import com.mustache.bbspractice2.domain.entity.CommentEntity;
 import com.mustache.bbspractice2.domain.entity.HospitalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<HospitalEntity,Integer> {
+    List<HospitalEntity> findByHospitalName(String hospitalName);
+
 }
