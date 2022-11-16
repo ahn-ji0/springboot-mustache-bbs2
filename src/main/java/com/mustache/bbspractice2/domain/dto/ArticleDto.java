e0f19ba5cc9e4b26ea2a4f7db6d14bd28c1de4a7
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-16 13:10:02 +0900

    Add: id 추가, @builder 추가, 생성자 추가

--- src/main/java/com/mustache/bbspractice2/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbspractice2/domain/dto/ArticleDto.java
@@ -2,13 +2,21 @@ package com.mustache.bbspractice2.domain.dto;
 
 import com.mustache.bbspractice2.domain.entity.ArticleEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ArticleDto {
+    private Long id;
     private String title;
     private String content;
+
+    public ArticleDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
     public ArticleEntity toEntity(){
         return new ArticleEntity(title,content);
