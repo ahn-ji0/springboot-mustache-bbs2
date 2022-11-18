9ba5607b22f36a3c63c49e1ef501e3dc505900c8
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-18 14:40:43 +0900

    Add: 지역이름으로 검색하는 기능 추가

--- src/main/java/com/mustache/bbspractice2/respository/HospitalRepository.java
+++ src/main/java/com/mustache/bbspractice2/respository/HospitalRepository.java
@@ -3,6 +3,8 @@ package com.mustache.bbspractice2.respository;
 import com.mustache.bbspractice2.domain.entity.ArticleEntity;
 import com.mustache.bbspractice2.domain.entity.CommentEntity;
 import com.mustache.bbspractice2.domain.entity.HospitalEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,7 @@ import java.util.List;
 public interface HospitalRepository extends JpaRepository<HospitalEntity,Integer> {
     List<HospitalEntity> findByBusinessTypeNameIn(List<String> businessTypes);
     List<HospitalEntity> findByRoadNameAddressContaining(String type);
+    Page<HospitalEntity> findByRoadNameAddressContaining(String name, Pageable pageable);
     List<HospitalEntity> findByHospitalNameStartsWith(String start);
     List<HospitalEntity> findByHospitalNameEndsWith(String end);
     List<HospitalEntity> findByTotalNumberOfBedsBetween(int min, int max);
