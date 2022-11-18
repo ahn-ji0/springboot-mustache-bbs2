789cd0ca5c7bae23723e233a110ae770a29f64f9
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-18 11:40:21 +0900

    Add: 비즈니스 로직-폐업 테스트 추가

--- src/test/java/com/mustache/bbspractice2/service/HospitalServiceTest.java
+++ src/test/java/com/mustache/bbspractice2/service/HospitalServiceTest.java
@@ -4,6 +4,7 @@ import com.mustache.bbspractice2.domain.dto.HospitalResponse;
 import com.mustache.bbspractice2.domain.entity.HospitalEntity;
 import com.mustache.bbspractice2.respository.HospitalRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -25,6 +26,7 @@ class HospitalServiceTest {
     }
 
     @Test
+    @DisplayName("영업중 테스트")
     void getHospital(){
         Integer id = 3;
         HospitalEntity hospitalEntity = new HospitalEntity(3,"광주광역시 북구 설죽로 495, 3층 (일곡동)","사랑이가득한치과의원","치과의원",0,13);
@@ -34,7 +36,17 @@ class HospitalServiceTest {
         HospitalResponse hospitalResponse = hospitalService.getHospital(id);
         assertEquals("사랑이가득한치과의원",hospitalResponse.getHospitalName());
         assertEquals("영업중",hospitalResponse.getBusinessStatusName());
-
+    }
+    @Test
+    @DisplayName("폐업 테스트")
+    void getHospital2(){
+        Integer id = 3;
+        HospitalEntity hospitalEntity = new HospitalEntity(3,"광주광역시 북구 설죽로 495, 3층 (일곡동)","사랑이가득한치과의원","치과의원",0,3);
+        Mockito.when(hospitalRepository.findById(id))
+                .thenReturn(Optional.of(hospitalEntity));
+        HospitalResponse hospitalResponse = hospitalService.getHospital(id);
+        assertEquals("사랑이가득한치과의원",hospitalResponse.getHospitalName());
+        assertEquals("폐업",hospitalResponse.getBusinessStatusName());
     }
 
 }
