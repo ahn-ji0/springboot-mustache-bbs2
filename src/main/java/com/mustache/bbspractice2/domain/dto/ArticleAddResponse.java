69b246a628051c7e76cb0bfa6b002d08db701dc3
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-20 23:08:07 +0900

    Update: builder 어노테이션 추가

--- src/main/java/com/mustache/bbspractice2/domain/dto/ArticleAddResponse.java
+++ src/main/java/com/mustache/bbspractice2/domain/dto/ArticleAddResponse.java
@@ -1,10 +1,12 @@
 package com.mustache.bbspractice2.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ArticleAddResponse {
     private Long id;
     private String title;
